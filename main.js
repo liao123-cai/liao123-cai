0b08fcd6d1cc5b89f6b436123585e30f306ad678
liao <14252354+liao_cai@user.noreply.gitee.com>
2024-05-13 17:07:49 +0800

    axios基础封装

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
-import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import ElementPlus from 'element-plus'
 
 import App from './App.vue'
 import router from './router'
+
+
+//测试接口函数
+import {getCategory} from '@/apis/testAPI'
+getCategory().then(res=>{
+    console.log(res)
+})
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(ElementPlus, { size: 'small', zIndex: 3000 })
 app.mount('#app')
