eca7b8a178f87f4922023f46ffa37df2aa28fc11
liao <14252354+liao_cai@user.noreply.gitee.com>
2024-05-13 17:58:10 +0800

    scss文件的自动导入

--- App.vue
+++ App.vue
@@ -1,7 +1,15 @@
 <template>
 <!-- 一级路由出口-->
 <router-view></router-view>
+<div class="test">
+test scss
+</div>
 </template>
 <script>
 
 </script>
+<style scoped lang="scss">
+.test{
+    color:$priceColor;
+}
+</style>
